8458d27b8b71900c36933ca4670bf92b31cad8d4
imali2 <193725552+imali2@users.noreply.github.com>
2025-04-26 16:13:32 +0500

    added new footer

--- resources/views/themes/xylo/layouts/footer.blade.php
+++ resources/views/themes/xylo/layouts/footer.blade.php
@@ -1,50 +1,52 @@
-<footer class="xylo-footer">
-        <div class="container py-4">
-            <!-- Top Row -->
-            <div class="row align-items-center py-5">
-                <!-- Left Links -->
-                <div class="col-md-4 d-flex justify-content-center justify-content-md-start mb-2 mb-md-0">
-                    <ul class="footer-links list-unstyled d-flex mb-0">
-                        <li><a href="#">TERMS & CONDITION</a></li>
-                        <li><a href="#">PRIVACY POLICY</a></li>
-                        <li><a href="#">DISCLAIMER</a></li>
-                    </ul>
-                </div>
+<footer class="bg-light pt-5">
+  <div class="container">
+    <div class="row">
+      <!-- Column 1: Logo -->
+      <div class="col-12 col-md-3 mb-4">
+        <img src="https://i.ibb.co/dHx2ZR3/velstore.png" alt="Velstore Logo" class="img-fluid" style="max-width: 100px;">
+      </div>
 
-                <!-- Center Brand -->
-                <div class="col-md-4 text-center mb-2 mb-md-0">
-                    <img src="assets/images/footer-logo.png" alt="">
-                </div>
+      <!-- Column 2: Account -->
+      <div class="col-6 col-md-3 mb-4">
+        <h5>Account</h5>
+        <ul class="list-unstyled">
+          <li class="mb-2"><a href="#" class="text-muted text-decoration-none">My Account</a></li>
+          <li class="mb-2"><a href="#" class="text-muted text-decoration-none">Wishlist</a></li>
+        </ul>
+      </div>
 
-                <!-- Right Links & Icons -->
-                <div class="col-md-4 d-flex justify-content-center justify-content-md-end">
-                    <ul class="footer-links list-unstyled d-flex mb-0">
-                        <li><a href="#">SIGN UP</a></li>
-                        <li><a href="#">LOGIN</a></li>
-                        <li>
-                            <a href="#"><i class="fab fa-facebook-f"></i></a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fab fa-instagram"></i></a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fab fa-youtube"></i></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+      <!-- Column 3: Other Pages -->
+      <div class="col-6 col-md-3 mb-4">
+        <h5>Pages</h5>
+        <ul class="list-unstyled">
+          <li class="mb-2"><a href="#" class="text-muted text-decoration-none">Privacy Policy</a></li>
+          <li class="mb-2"><a href="#" class="text-muted text-decoration-none">Terms of Service</a></li>
+        </ul>
+      </div>
 
-            <!-- Bottom Bar -->
-            <div class="row mt-3">
-                <div class="col-md-3 text-center text-md-start">
-                    <p class="m-0">&copy; 2025 Xylo. All Rights Reserved</p>
-                </div>
-                <div class="col-md-7">
-                    <img src="assets/images/footerbotom-line.png" alt="" class="w-100">
-                </div>
-                <div class="col-md-2 text-center text-md-end">
-                    <p class="m-0">Are You Interested?</p>
-                </div>
-            </div>
+      <!-- Column 4: Social Links -->
+    <div class="col-12 col-md-3 mb-4">
+    <h5>Follow Us</h5>
+    <div class="d-flex gap-3">
+        <a href="#" class="text-dark fs-5"><i class="fab fa-facebook-f"></i></a>
+        <a href="#" class="text-dark fs-5"><i class="fab fa-twitter"></i></a>
+        <a href="#" class="text-dark fs-5"><i class="fab fa-instagram"></i></a>
+        <a href="#" class="text-dark fs-5"><i class="fab fa-linkedin-in"></i></a>
+    </div>
+    </div>
+
+    </div>
+  </div>
+
+  <!-- Footer Bottom Strip -->
+  <div class="bg-black text-white py-3 mt-4">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 d-flex justify-content-between flex-wrap small">
+          <span>© 2025 Velstore Demo. All Rights Reserved.</span>
+          <span>Powered by Velstore Labs</span>
         </div>
-    </footer>
+      </div>
+    </div>
+  </div>
+</footer>
