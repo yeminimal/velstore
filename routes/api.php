2e24ca8890c784ab4bdff6a257aa65b33581b511
asadshahdev <asadshahdev@gmail.com>
2025-05-22 18:40:00 +0500

    add GET /api/banners endpoint to list all promotional banners with image URLs

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CustomerAuthController;
+use App\Http\Controllers\Api\BannerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,4 +30,6 @@ Route::prefix('customer')->group(function () {
         Route::post('logout', [CustomerAuthController::class, 'logout']);
     });
 });
+
+Route::get('/banners', [BannerController::class, 'index']);
 
