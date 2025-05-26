cb974f29063e767c7746863284fd5225da98b61a
imali2 <193725552+imali2@users.noreply.github.com>
2025-05-26 09:31:26 +0500

    Update BrandController.php

--- app/Http/Controllers/Api/BrandController.php
+++ app/Http/Controllers/Api/BrandController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\Brand;
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 
 class BrandController extends Controller
 {
-        
     public function index(Request $request)
     {
         $language = $request->get('lang', App::getLocale());
@@ -17,20 +16,20 @@ class BrandController extends Controller
         $brands = Brand::with(['translations' => function ($query) use ($language) {
             $query->where('locale', $language);
         }])
-        ->where('status', 'active') 
-        ->get()
-        ->map(function ($brand) use ($language) {
-            $translation = $brand->translations->first();
+            ->where('status', 'active')
+            ->get()
+            ->map(function ($brand) use ($language) {
+                $translation = $brand->translations->first();
 
-            return [
-                'id' => $brand->id,
-                'slug' => $brand->slug,
-                'logo_url' => $brand->logo_url,
-                'status' => $brand->status,
-                'name' => $translation->name ?? null,
-                'description' => $translation->description ?? null,
-            ];
-        });
+                return [
+                    'id' => $brand->id,
+                    'slug' => $brand->slug,
+                    'logo_url' => $brand->logo_url,
+                    'status' => $brand->status,
+                    'name' => $translation->name ?? null,
+                    'description' => $translation->description ?? null,
+                ];
+            });
 
         return response()->json([
             'status' => true,
