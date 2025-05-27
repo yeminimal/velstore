8aaa98b8cf0602f12509aab5b7b52a4b8dc62740
imali2 <193725552+imali2@users.noreply.github.com>
2025-05-27 10:01:37 +0500

    Update SocialMediaLinkController.php

--- app/Http/Controllers/Api/SocialMediaLinkController.php
+++ app/Http/Controllers/Api/SocialMediaLinkController.php
@@ -9,23 +9,26 @@ use Illuminate\Support\Facades\App;
 
 class SocialMediaLinkController extends Controller
 {
-     public function index(Request $request)
+    public function index(Request $request)
     {
         $lang = $request->get('lang', App::getLocale());
 
-        $links = SocialMediaLink::with(['translations' => function ($query) use ($lang) {
-            $query->where('language_code', $lang);
-        }])->get();
+        $links = SocialMediaLink::with([
+                'translations' => function ($query) use ($lang) {
+                    $query->where('language_code', $lang);
+                },
+            ])
+            ->get();
 
         $data = $links->map(function ($link) use ($lang) {
             $translation = $link->translations->first();
 
             return [
-                'id'       => $link->id,
-                'type'     => $link->type,
+                'id' => $link->id,
+                'type' => $link->type,
                 'platform' => $link->platform,
-                'link'     => $link->link,
-                'name'     => $translation ? $translation->name : null,
+                'link' => $link->link,
+                'name' => $translation ? $translation->name : null,
             ];
         });
 
