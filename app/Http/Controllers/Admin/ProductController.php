b250bfd3a5419026bf8ef3301c039b586bf4f2d3
imali2 <193725552+imali2@users.noreply.github.com>
2025-08-17 12:57:36 +0500

    Update ProductController.php

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Attribute;
 use App\Models\AttributeValue;
 use App\Models\Brand;
 use App\Models\Category;
+use App\Models\Language;
 use App\Models\Product;
 use App\Models\ProductAttributeValue;
 use App\Models\Vendor;
@@ -13,6 +15,7 @@ use App\Services\Admin\CategoryService;
 use App\Services\Admin\ProductService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class ProductController extends Controller
